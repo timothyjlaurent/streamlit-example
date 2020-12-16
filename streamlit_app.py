7527f4a8bdedfdcb00421ee8b046e8ab4511a514
Timothy Laurent <timothy.laurent@invitae.com>
2020-12-15 23:05:07 -0800

    add model add predict

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,4 +35,6 @@ with st.echo(code_location='below'):
         image = PIL.Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.')
         predictions = model.predict(PILImage(image))
+        f"Predicted Emotion: **{predictions[0]}**"
+        st.bar_chart(pd.DataFrame(predictions[2], columns=model.vocab))
         predictions
